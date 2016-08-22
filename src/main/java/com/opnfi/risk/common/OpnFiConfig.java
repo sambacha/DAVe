cf314ba1971ab444c668624b4b4154a26e74eaf8
Michal Zerola <michal.zerola@gmail.com>
2016-08-22 16:00:15 +0200

    Configurable ERS connection options

--- src/main/java/com/opnfi/risk/common/OpnFiConfig.java
+++ src/main/java/com/opnfi/risk/common/OpnFiConfig.java
@@ -8,4 +8,24 @@ public interface OpnFiConfig extends Config {
     @DefaultValue("8080")
     public int httpPort();
 
+    @DefaultValue("localhost")
+    public String ersBrokerHost();
+
+    @DefaultValue("5762")
+    public int ersBrokerPort();
+
+    @DefaultValue("alias")
+    public String sslCertAlias();
+
+    @DefaultValue("truststore")
+    public String truststore();
+
+    @DefaultValue("123456")
+    public String truststorePassword();
+
+    @DefaultValue("keystore")
+    public String keystore();
+
+    @DefaultValue("123456")
+    public String keystorePassword();
 }
