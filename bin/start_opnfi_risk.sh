8430ca46178ff3a2fa18e315dd080d2b34798fa7
Michal Zerola <michal.zerola@gmail.com>
2016-08-29 16:31:39 +0200

    Replacing log4j by logback. Addressing #7

--- bin/start_opnfi_risk.sh
+++ bin/start_opnfi_risk.sh
@@ -7,5 +7,8 @@ fi
 
 OPNFI_RISK_LIB=${OPNFI_RISK_ROOT}/lib
 OPNFI_RISK_ETC=${OPNFI_RISK_ROOT}/etc
+export OPNFI_RISK_LOG=${OPNFI_RISK_ROOT}/log
 
-java -Dvertx.logger-delegate-factory-class-name=io.vertx.core.logging.SLF4JLogDelegateFactory -jar ${OPNFI_RISK_LIB}/risk-1.0-SNAPSHOT-fat.jar -conf ${OPNFI_RISK_ETC}/opnfi-risk.json
+java -Dvertx.logger-delegate-factory-class-name=io.vertx.core.logging.SLF4JLogDelegateFactory \
+     -Dlogback.configurationFile=${OPNFI_RISK_ETC}/logback.xml \
+     -jar ${OPNFI_RISK_LIB}/risk-1.0-SNAPSHOT-fat.jar -conf ${OPNFI_RISK_ETC}/opnfi-risk.json
