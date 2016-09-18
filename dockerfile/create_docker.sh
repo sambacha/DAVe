520e369d751416cea3d1277eb54cf267c94f28f5
Jakub Scholz <www@scholzj.com>
2016-09-18 19:09:02 +0200

    Add ERS image certifdicates to assemly

--- dockerfile/create_docker.sh
+++ dockerfile/create_docker.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 cp -r -v ./target/dave-1.0-SNAPSHOT/dave-1.0-SNAPSHOT ./dockerfile/dave-1.0-SNAPSHOT
 cp -v ./dockerfile/dave.json ./dockerfile/dave-1.0-SNAPSHOT/etc/dave.json
-cp -v ./dockerfile/abcfr.keystore ./dockerfile/dave-1.0-SNAPSHOT/etc/abcfr.keystore
-cp -v ./dockerfile/deffr.keystore ./dockerfile/dave-1.0-SNAPSHOT/etc/deffr.keystore
-cp -v ./dockerfile/ersd01.truststore ./dockerfile/dave-1.0-SNAPSHOT/etc/ersd01.truststore
+#cp -v ./dockerfile/abcfr.keystore ./dockerfile/dave-1.0-SNAPSHOT/etc/abcfr.keystore
+#cp -v ./dockerfile/deffr.keystore ./dockerfile/dave-1.0-SNAPSHOT/etc/deffr.keystore
+#cp -v ./dockerfile/ersd01.truststore ./dockerfile/dave-1.0-SNAPSHOT/etc/ersd01.truststore
 docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 docker build -t scholzj/dave:latest ./dockerfile/
-docker tag scholzj/dave:latest docker.io/scholzj/dave:latest
+docker tag -f scholzj/dave:latest docker.io/scholzj/dave:latest
 docker push scholzj/dave:latest
