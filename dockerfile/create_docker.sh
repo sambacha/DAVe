bb0347b5c21e214d73831af5e36da0c3b480794e
Jakub Scholz <www@scholzj.com>
2016-09-18 19:51:27 +0200

    Push images under commit hash

--- dockerfile/create_docker.sh
+++ dockerfile/create_docker.sh
@@ -5,6 +5,6 @@ cp -v ./dockerfile/dave.json ./dockerfile/dave-1.0-SNAPSHOT/etc/dave.json
 #cp -v ./dockerfile/deffr.keystore ./dockerfile/dave-1.0-SNAPSHOT/etc/deffr.keystore
 #cp -v ./dockerfile/ersd01.truststore ./dockerfile/dave-1.0-SNAPSHOT/etc/ersd01.truststore
 docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-docker build -t scholzj/dave:latest ./dockerfile/
-docker tag -f scholzj/dave:latest docker.io/scholzj/dave:latest
-docker push scholzj/dave:latest
+docker build -t scholzj/dave:${CIRCLE_SHA1} ./dockerfile/
+docker tag -f scholzj/dave:${CIRCLE_SHA1} docker.io/scholzj/dave:${CIRCLE_SHA1}
+docker push scholzj/dave:${CIRCLE_SHA1}
