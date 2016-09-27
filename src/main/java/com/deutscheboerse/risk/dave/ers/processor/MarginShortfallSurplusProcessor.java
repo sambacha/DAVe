f49de8eac93f639834bd5bcce08264a859e8cabf
Jakub Scholz <jakub@scholz.cz>
2016-09-27 10:50:15 +0200

    Process received acknowledgement messages and stop the exchange #8

--- src/main/java/com/deutscheboerse/risk/dave/ers/processor/MarginShortfallSurplusProcessor.java
+++ src/main/java/com/deutscheboerse/risk/dave/ers/processor/MarginShortfallSurplusProcessor.java
@@ -1,9 +1,6 @@
 package com.deutscheboerse.risk.dave.ers.processor;
 
-import com.deutscheboerse.risk.dave.ers.jaxb.MarginRequirementReportMessageT;
-import com.deutscheboerse.risk.dave.ers.jaxb.AbstractMessageT;
-import com.deutscheboerse.risk.dave.ers.jaxb.FIXML;
-import com.deutscheboerse.risk.dave.ers.jaxb.MarginAmountBlockT;
+import com.deutscheboerse.risk.dave.ers.jaxb.*;
 import io.vertx.core.json.JsonObject;
 
 import java.util.Collections;
@@ -14,16 +11,31 @@ import java.util.Set;
 
 import javax.xml.bind.JAXBElement;
 
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
 import org.apache.camel.Exchange;
+import org.apache.camel.ExchangePattern;
 import org.apache.camel.Message;
 import org.apache.camel.Processor;
 
 public class MarginShortfallSurplusProcessor extends AbstractProcessor implements Processor {
+    private static final Logger LOG = LoggerFactory.getLogger(MarginShortfallSurplusProcessor.class);
 
-    private JsonObject parseFromFIXML(FIXML fixml) {
+    private JsonObject parseFromFIXML(FIXML fixml) throws Exception {
         JAXBElement<? extends AbstractMessageT> msg = fixml.getMessage();
-        MarginRequirementReportMessageT mrrMessage = (MarginRequirementReportMessageT) msg.getValue();
 
+        if (msg.getValue() instanceof MarginRequirementReportMessageT) {
+            return processMarginRequirementReport((MarginRequirementReportMessageT)msg.getValue());
+        }
+        else
+        {
+            processMarginRequirementInqAck((MarginRequirementInquiryAckMessageT)msg.getValue());
+            throw new Exception("Something went wrong");
+        }
+    }
+
+    private JsonObject processMarginRequirementReport(MarginRequirementReportMessageT mrrMessage)
+    {
         JsonObject mss = new JsonObject();
         mss.put("received", new JsonObject().put("$date", timestampFormatter.format(new Date())));
         mss.put("reqId", mrrMessage.getID());
@@ -48,9 +60,44 @@ public class MarginShortfallSurplusProcessor extends AbstractProcessor implement
         return mss;
     }
 
+    private void processMarginRequirementInqAck(MarginRequirementInquiryAckMessageT ackMessage) throws Exception {
+        // <?xml version="1.0" encoding="UTF-8" standalone="yes"?><FIXML>
+        //    <MgnReqmtInqAck ID="bb183b6a-9d13-4e2a-b2b8-152da2917a31" Stat="4" Rslt="307" SetSesID="ITD" TxnTm="2013-12-18T10:32:09.29" Txt="Unknown pool ID.">
+        //        <MgnReqmtInqQual Qual="2"/>
+        //        <Pty ID="CBKFR" Src="D" R="4">
+        //            <Sub ID="" Typ="4000"/>
+        //        </Pty>
+        //        <Pty ID="CARLO" Src="D" R="1"/>
+        //    </MgnReqmtInqAck>
+        // </FIXML>
+
+        // TODO: Add tests for this kind of messages
+
+        String result = ackMessage.getRslt();
+        String error = ackMessage.getTxt();
+
+        switch (result)
+        {
+            case "307":
+                LOG.info("Received MarginRequirementInquiryAcknowledgement with result {} and error message {}", result, error);
+                break;
+            default:
+                LOG.error("Received MarginRequirementInquiryAcknowledgement with result {} and error message {}", result, error);
+                throw new Exception();
+        }
+    }
+
    @Override
    public void process(Exchange exchange) {
-        Message in = exchange.getIn();
-        in.setBody(this.parseFromFIXML((FIXML)in.getBody()));
+       Message in = exchange.getIn();
+
+       try {
+           in.setBody(parseFromFIXML((FIXML) in.getBody()));
+       }
+       catch (Exception e)
+       {
+           // Stop the exchange
+           exchange.setProperty(Exchange.ROUTE_STOP, Boolean.TRUE);
+       }
     }
 }
