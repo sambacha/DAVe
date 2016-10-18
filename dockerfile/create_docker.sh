950ecfcf10db3ed996d2280b6dd837c4a2a9aad5
Jakub Scholz <jakub@scholz.cz>
2016-10-18 17:15:47 +0200

    Add first version of the entrypoint which configures HTTP, DB and AUTH. This also updates the deployment, which now uses the new env variables. #22

--- dockerfile/create_docker.sh
+++ dockerfile/create_docker.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 cp -r -v ./target/dave-1.0-SNAPSHOT/dave-1.0-SNAPSHOT ./dockerfile/dave-1.0-SNAPSHOT
-cp -v ./dockerfile/dave.json ./dockerfile/dave-1.0-SNAPSHOT/etc/dave.json
+rm -r ./dockerfile/dave-1.0-SNAPSHOT/etc/dave.json ./dockerfile/dave-1.0-SNAPSHOT/etc/*.keystore ./dockerfile/dave-1.0-SNAPSHOT/etc/*.truststore ./dockerfile/dave-1.0-SNAPSHOT/etc/truststore
 #cp -v ./dockerfile/abcfr.keystore ./dockerfile/dave-1.0-SNAPSHOT/etc/abcfr.keystore
 #cp -v ./dockerfile/deffr.keystore ./dockerfile/dave-1.0-SNAPSHOT/etc/deffr.keystore
 #cp -v ./dockerfile/ersd01.truststore ./dockerfile/dave-1.0-SNAPSHOT/etc/ersd01.truststore
