00311b80decd2d20cfe2b875f6cf36de3d67ec15
Jakub Scholz <jakub@scholz.cz>
2016-10-18 17:17:45 +0200

    Push the latest docker image also under the nightly tag, so that we can easily refer the last build from outsire. Latest should point to the latest stable #22

--- dockerfile/create_docker.sh
+++ dockerfile/create_docker.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
+
+# Copy the DAVe binaries
 cp -r -v ./target/dave-1.0-SNAPSHOT/dave-1.0-SNAPSHOT ./dockerfile/dave-1.0-SNAPSHOT
+
+# Delete the prefilled
 rm -r ./dockerfile/dave-1.0-SNAPSHOT/etc/dave.json ./dockerfile/dave-1.0-SNAPSHOT/etc/*.keystore ./dockerfile/dave-1.0-SNAPSHOT/etc/*.truststore ./dockerfile/dave-1.0-SNAPSHOT/etc/truststore
-#cp -v ./dockerfile/abcfr.keystore ./dockerfile/dave-1.0-SNAPSHOT/etc/abcfr.keystore
-#cp -v ./dockerfile/deffr.keystore ./dockerfile/dave-1.0-SNAPSHOT/etc/deffr.keystore
-#cp -v ./dockerfile/ersd01.truststore ./dockerfile/dave-1.0-SNAPSHOT/etc/ersd01.truststore
 docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 docker build -t scholzj/dave:${CIRCLE_SHA1} ./dockerfile/
 docker tag -f scholzj/dave:${CIRCLE_SHA1} docker.io/scholzj/dave:${CIRCLE_SHA1}
 docker push scholzj/dave:${CIRCLE_SHA1}
+docker tag -f scholzj/dave:${CIRCLE_SHA1} docker.io/scholzj/dave:nightly
+docker push scholzj/dave:nightly
