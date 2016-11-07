259971c5d278d52a6c0fc3721dfee80c0f37e7a3
Jakub Scholz <www@scholzj.com>
2016-11-07 22:07:47 +0100

    Add new dashboard tab with MC components agregated to account levels

--- src/main/resources/webroot/app/dashboard/dashboard.controller.js
+++ src/main/resources/webroot/app/dashboard/dashboard.controller.js
@@ -7,74 +7,14 @@
 
     angular.module('dave').controller('DashboardController', DashboardController);
 
-    function DashboardController($scope, $routeParams, $http, $interval, $filter) {
+    function DashboardController() {
         var vm = this;
-        vm.errorMessage = "";
-//        vm.chartData = {
-//            "MarginRequirement": [],
-//            "MarginShortfallSurplus": [],
-//            "MarginCall": []
-//        };
-//
-//        var refresh = $interval(loadData, 60000);
-//        var restQueryUrl = '/api/v1.0/mss/latest/';
         vm.activeTab = "cv";
         vm.setActiveTab = setActiveTab;
-
-//        loadData();
 
         ////////////////////
         function setActiveTab(tabName) {
             vm.activeTab = tabName;
         }
-
-        function loadData(){
-            $http.get(restQueryUrl).success(function(data) {
-                processGraphData(data);
-                vm.errorMessage = "";
-                vm.initialLoad = false;
-            }).error(function(data, status, headers, config) {
-                vm.errorMessage = "Server returned status " + status;
-                vm.initialLoad = false;
-            });
-        }
-
-        function processGraphData(data) {
-            var chartData = {
-                "MarginRequirement": [],
-                "MarginShortfallSurplus": [],
-                "MarginCall": []
-            };
-            var index;
-
-            for (index = 0; index < data.length; ++index) {
-                var marginRequirementTick = {
-                    label: data[index].pool + " / " + data[index].member + " / " + data[index].clearingCcy,
-                    value: data[index].marginRequirement
-                };
-
-                var marginShortfallSurplusTick = {
-                    label: data[index].pool + " / " + data[index].member + " / " + data[index].clearingCcy,
-                    value: data[index].shortfallSurplus
-                };
-
-                var marginCallTick = {
-                    label: data[index].pool + " / " + data[index].member + " / " + data[index].clearingCcy,
-                    value: data[index].marginCall
-                };
-
-                chartData.MarginRequirement.push(marginRequirementTick);
-                chartData.MarginShortfallSurplus.push(marginShortfallSurplusTick);
-                chartData.MarginCall.push(marginCallTick);
-            }
-
-            vm.chartData = chartData;
-        }
-
-//        $scope.$on("$destroy", function() {
-//            if (refresh != null) {
-//                $interval.cancel(refresh);
-//            }
-//        });
     };
 })();
---
+++ src/main/resources/webroot/app/margin-components/margin-component-latest-account-aggregation.controller.js
@@ -0,0 +1,89 @@
+/**
+ * Created by jakub on 20/10/2016.
+ */
+
+(function() {
+    'use strict';
+
+    angular.module('dave').controller('MarginComponentLatestAccountAggregationController', MarginComponentLatestAccountAggregationController);
+
+    function MarginComponentLatestAccountAggregationController($scope, $http, $interval, $filter, sortRecordsService) {
+        var vm = this;
+        vm.defaultOrdering = ["clearer", "member", "account"];
+        vm.ordering = vm.defaultOrdering;
+        vm.initialLoad= true;
+        vm.recordCount = 0;
+        vm.errorMessage = "";
+        vm.viewWindow = [];
+        vm.sortRecords = sortRecords;
+
+        var refresh = $interval(loadData, 60000);
+
+        loadData();
+
+        ////////////////////
+
+        function loadData() {
+            $http.get(getRestQueryUrl()).success(function(data) {
+                processData(data);
+                vm.errorMessage = "";
+                vm.initialLoad = false;
+            }).error(function(data, status, headers, config) {
+                vm.errorMessage = "Server returned status " + status;
+                vm.initialLoad = false;
+            });
+        }
+
+        function sortRecords(column) {
+            vm.ordering = sortRecordsService(column, vm.ordering, vm.defaultOrdering);
+            vm.viewWindow = $filter('orderBy')(vm.viewWindow, vm.ordering);
+        }
+
+        function processData(data) {
+            var index;
+            var newViewWindow = {};
+
+            for (index = 0; index < data.length; ++index) {
+                var fKey = functionalKey(data[index]);
+
+                if (fKey in newViewWindow)
+                {
+                    newViewWindow[fKey].variationMargin += data[index].variationMargin;
+                    newViewWindow[fKey].premiumMargin += data[index].premiumMargin;
+                    newViewWindow[fKey].liquiMargin += data[index].liquiMargin;
+                    newViewWindow[fKey].spreadMargin += data[index].spreadMargin;
+                    newViewWindow[fKey].additionalMargin += data[index].additionalMargin;
+                }
+                else {
+                    newViewWindow[fKey] = {
+                        "functionalKey": fKey,
+                        "clearer": data[index].clearer,
+                        "member": data[index].member,
+                        "account": data[index].account,
+                        "variationMargin": data[index].variationMargin,
+                        "premiumMargin": data[index].premiumMargin,
+                        "liquiMargin": data[index].liquiMargin,
+                        "spreadMargin": data[index].spreadMargin,
+                        "additionalMargin": data[index].additionalMargin
+                    };
+                }
+            }
+
+            vm.viewWindow = $filter('orderBy')(Object.keys(newViewWindow).map(function (key) { return newViewWindow[key]; }), vm.ordering);
+        }
+
+        function functionalKey(record) {
+            return record.clearer + '-' + record.member + '-' + record.account;
+        }
+
+        function getRestQueryUrl() {
+            return '/api/v1.0/mc/latest/';
+        }
+
+        $scope.$on("$destroy", function() {
+            if (refresh != null) {
+                $interval.cancel(refresh);
+            }
+        });
+    };
+})();
