c1e771b8e86c5287e3d77706ebff4ac13ef433ae
Adam Netocny <adam.netocny@deutsche-boerse.com>
2016-11-18 15:36:19 +0100

    Fix for missing cookies when using CORS.

--- src/main/resources/webroot/app/app.js
+++ src/main/resources/webroot/app/app.js
@@ -11,6 +11,10 @@
         'googlechart'
     ]);
 
+    dave.config(['$httpProvider', function($httpProvider) {
+        $httpProvider.defaults.withCredentials = true;
+    }]);
+
     dave.constant('hostConfig', {
         restURL: '/api/v1.0' // 'http(s)://someUrl:port/path'
     });
