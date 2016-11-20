361885e7b77c28ded3a76dfc7600fe323c472c08
Jakub Scholz <www@scholzj.com>
2016-11-20 01:13:38 +0100

    Route the deployment to the new cluster

--- deployment/kube_deploy.sh
+++ deployment/kube_deploy.sh
@@ -3,9 +3,8 @@
 curl -O https://storage.googleapis.com/kubernetes-release/release/v1.4.6/bin/linux/amd64/kubectl
 chmod +x kubectl
 
-./kubectl config set-cluster aws-dave --server ${KUBE_API}
-./kubectl config set-cluster aws-dave --insecure-skip-tls-verify=true
-./kubectl config set-credentials dave-admin --username=${KUBE_USERNAME} --token=${KUBE_PASSWORD}
-./kubectl config set-context dave --cluster=aws-dave --user=dave-admin
-./kubectl config use-context dave
+./kubectl config set-cluster dave --certificate-authority=/var/lib/kubernetes/ca.pem --embed-certs=true --server=${KUBE_API}
+./kubectl config set-credentials ${KUBE_USERNAME} --token ${KUBE_PASSWORD}
+./kubectl config set-context default-context --cluster=dave --user=${KUBE_USERNAME}
+./kubectl config use-context default-context
 ./kubectl set image deployment/dave-deployment dave=scholzj/dave:${CIRCLE_SHA1}
