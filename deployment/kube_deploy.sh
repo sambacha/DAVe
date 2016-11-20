8880fa07556db9b0b4043543eca93776d4382291
Jakub Scholz <www@scholzj.com>
2016-11-20 01:20:21 +0100

    Fix certificate issue

--- deployment/kube_deploy.sh
+++ deployment/kube_deploy.sh
@@ -3,7 +3,7 @@
 curl -O https://storage.googleapis.com/kubernetes-release/release/v1.4.6/bin/linux/amd64/kubectl
 chmod +x kubectl
 
-./kubectl config set-cluster dave --certificate-authority=/var/lib/kubernetes/ca.pem --embed-certs=true --server=${KUBE_API}
+./kubectl config set-cluster dave --insecure-skip-tls-verify=true --server=${KUBE_API}
 ./kubectl config set-credentials ${KUBE_USERNAME} --token ${KUBE_PASSWORD}
 ./kubectl config set-context default-context --cluster=dave --user=${KUBE_USERNAME}
 ./kubectl config use-context default-context
