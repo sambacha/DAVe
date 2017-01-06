de8fb19cc419f355b747c753ef2494f5aa54d30b
Adam Netocny <adam.netocny.ext@deutsche-boerse.com>
2017-01-06 14:56:32 +0100

    Added filtering

--- src/main/resources/webroot/app/abstract.list.component.ts
+++ src/main/resources/webroot/app/abstract.list.component.ts
@@ -21,6 +21,8 @@ export abstract class AbstractListComponent<T> extends AbstractComponentWithAuto
 
     public data: T[];
 
+    private sourceData: T[];
+
     public pageSize: number = 20;
 
     constructor(private route: ActivatedRoute) {
@@ -67,12 +69,13 @@ export abstract class AbstractListComponent<T> extends AbstractComponentWithAuto
             this.data.push(data[index]);
         }
 
+        this.sourceData = this.data;
+
         this.filter();
 
         delete this.errorMessage;
         this.initialLoad = false;
     }
-
 
     public filterAfterTimeout() {
         clearTimeout(this.filterTimeoutHandle);
@@ -82,7 +85,38 @@ export abstract class AbstractListComponent<T> extends AbstractComponentWithAuto
     }
 
     public filter(): void {
+        if (this.filterQuery) {
+            let filters: string[] = this.filterQuery.toLowerCase().split(" ");
+            let index: number;
+            let index2: number;
+            let filteredItems: T[] = [];
 
+            for (index = 0; index < this.sourceData.length; index++) {
+                let match = true;
+
+                for (index2 = 0; index2 < filters.length; index2++) {
+                    if (!MatchObject(this.sourceData[index], filters[index2])) {
+                        match = false;
+                        break;
+                    }
+                }
+
+                if (match == true) {
+                    filteredItems.push(this.sourceData[index]);
+                }
+            }
+
+            this.data = filteredItems;
+        }
+        else {
+            this.data = this.sourceData;
+        }
+
+        function MatchObject(item: any, search: string): boolean {
+            return Object.keys(item).some((key: string) => {
+                return String(item[key]).toLowerCase().indexOf(search) !== -1;
+            });
+        }
     }
 
 }
