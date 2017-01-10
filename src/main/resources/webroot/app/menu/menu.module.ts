2f54ecad88d8bdd9089a84b564d735b173dfdc96
Adam Netocny <adam.netocny.ext@deutsche-boerse.com>
2017-01-10 14:31:29 +0100

    Highlight of "history" routes.

--- src/main/resources/webroot/app/menu/menu.module.ts
+++ src/main/resources/webroot/app/menu/menu.module.ts
@@ -3,13 +3,19 @@ import {BrowserModule} from '@angular/platform-browser';
 import {RouterModule} from '@angular/router';
 
 import {MenuComponent} from './menu.component';
+import {RouterLinkActiveDirective} from './router.link.active.directive';
+import {RouterSubLinkDirective} from './router.sub.link.directive';
 
 @NgModule({
     imports: [
         BrowserModule,
         RouterModule
     ],
-    declarations: [MenuComponent],
+    declarations: [
+        MenuComponent,
+        RouterLinkActiveDirective, // Allows us to highlight also sub routes like "history" tabs.
+        RouterSubLinkDirective
+    ],
     exports: [MenuComponent]
 })
 export class MenuModule {
