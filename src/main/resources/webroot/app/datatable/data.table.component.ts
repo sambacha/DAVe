313eeb21c1599f21cdb97154121518ee818bce6c
Adam Netocny <adam.netocny.ext@deutsche-boerse.com>
2017-01-12 11:32:42 +0100

    Colspan in position reports history (detail row)

--- src/main/resources/webroot/app/datatable/data.table.component.ts
+++ src/main/resources/webroot/app/datatable/data.table.component.ts
@@ -1,24 +1,10 @@
-import {Component, Input, OnChanges, QueryList, ContentChildren, TemplateRef, SimpleChanges} from '@angular/core';
+import {Component, Input, OnChanges, QueryList, ContentChildren, SimpleChanges} from '@angular/core';
 
 import {DataTableColumnDirective} from './data.table.column.directive';
 import {DataTableRowDetailDirective} from './data.table.row.detail.directive';
 import {DataTableColumnGroupDirective} from './data.table.column.group.directive';
 
-export interface DataTableColumn {
-    title?: string;
-    sortingKey?: string;
-    tooltip?: string;
-    cellTemplate?: TemplateRef<any>,
-    footerTemplate?: TemplateRef<any>,
-    rowspan?: number;
-    colspan?: number;
-}
-
-interface DataTableDefinition {
-    rowsTemplates: DataTableColumn[][];
-    headerTemplates: DataTableColumn[][];
-    footerTemplates: DataTableColumn[][];
-}
+import {DataTableDefinition, DataTableCell, DataTableUtils} from './data.table.utils';
 
 @Component({
     moduleId: module.id,
@@ -51,16 +37,6 @@ export class DataTableComponent implements OnChanges {
     private currentPage: number = 1;
 
     private ordering: string[];
-
-    @ContentChildren(DataTableRowDetailDirective, {descendants: false})
-    public _rowDetailTemplate: QueryList<DataTableRowDetailDirective>;
-
-    @ContentChildren(DataTableColumnDirective, {descendants: false})
-    public _columnTemplates: QueryList<DataTableColumnDirective>;
-
-    private tableDefinition: DataTableDefinition;
-
-    private rowDetailTableDefinitions: DataTableDefinition[];
 
     public ngOnChanges(changes: SimpleChanges): void {
         this.sort();
@@ -132,62 +108,55 @@ export class DataTableComponent implements OnChanges {
         this.updatePage(this.currentPage);
     }
 
-    public get headerTemplates(): DataTableColumn[][] {
-        if (this._columnTemplates && !this.tableDefinition) {
-            this.tableDefinition = this.computeSpans(this._columnTemplates.toArray());
+    //<editor-fold defaultstate="collapsed" desc="Template processing">
+
+    @ContentChildren(DataTableRowDetailDirective, {descendants: false})
+    public _rowDetailTemplate: QueryList<DataTableRowDetailDirective>;
+
+    @ContentChildren(DataTableColumnDirective, {descendants: false})
+    public _columnTemplates: QueryList<DataTableColumnDirective>;
+
+    private _tableDefinition: DataTableDefinition;
+
+    private rowDetailTableDefinitions: DataTableDefinition[];
+
+    private get tableDefinition(): DataTableDefinition {
+        if (this._columnTemplates && !this._tableDefinition) {
+            this._tableDefinition = DataTableUtils.computeSpans(this._columnTemplates.toArray());
         }
+        return this._tableDefinition;
+    }
+
+    public get headerTemplates(): DataTableCell[][] {
         return this.tableDefinition.headerTemplates;
     }
 
-    public get rowsTemplates(): DataTableColumn[][] {
-        if (this._columnTemplates && !this.tableDefinition) {
-            this.tableDefinition = this.computeSpans(this._columnTemplates.toArray());
-        }
+    public get rowsTemplates(): DataTableCell[][] {
         return this.tableDefinition.rowsTemplates;
     }
 
-    public get footerTemplates(): DataTableColumn[][] {
-        if (this._columnTemplates && !this.tableDefinition) {
-            this.tableDefinition = this.computeSpans(this._columnTemplates.toArray());
-        }
+    public get footerTemplates(): DataTableCell[][] {
         return this.tableDefinition.footerTemplates;
     }
 
     public get detailRowColspan(): number {
-        let template: DataTableColumn[];
-        if (this.headerTemplates && this.headerTemplates.length) {
-            template = this.headerTemplates[0];
-        } else if (this.rowsTemplates && this.rowsTemplates.length) {
-            template = this.rowsTemplates[0];
-        } else if (this.footerTemplates && this.footerTemplates.length) {
-            template = this.footerTemplates[0];
-        }
-
-        if (template) {
-            let colspan: number = 0;
-            template.forEach((cell: DataTableColumn) => {
-                if (cell.colspan) {
-                    colspan += cell.colspan;
-                } else {
-                    colspan++;
-                }
-            });
-            return colspan;
-        }
-        return 1;
+        return DataTableUtils.getColumnsCountForTemplate(this.tableDefinition);
     }
 
     public get detailRowGroups(): DataTableDefinition[] {
         if (this._rowDetailTemplate && this._rowDetailTemplate.first && !this.rowDetailTableDefinitions) {
             let detailTemplate: DataTableRowDetailDirective = this._rowDetailTemplate.first;
             let rowDetailTableDefinitions: DataTableDefinition[] = [];
+            let maxColspan: number = 0;
             detailTemplate.columnGroups.forEach((item: DataTableColumnGroupDirective) => {
-                rowDetailTableDefinitions.push(this.computeSpans(item.columns.toArray()));
+                let definition: DataTableDefinition = DataTableUtils.computeSpans(item.columns.toArray());
+                maxColspan = Math.max(maxColspan, DataTableUtils.getColumnsCountForTemplate(definition));
+                rowDetailTableDefinitions.push(definition);
             });
             if (!rowDetailTableDefinitions.length) {
                 delete this.rowDetailTableDefinitions;
             } else {
-
+                DataTableUtils.fixColspans(rowDetailTableDefinitions, maxColspan);
 
                 this.rowDetailTableDefinitions = rowDetailTableDefinitions;
             }
@@ -195,203 +164,6 @@ export class DataTableComponent implements OnChanges {
         return this.rowDetailTableDefinitions;
     }
 
-    private computeSpans(columnDirectives: DataTableColumnDirective[]): DataTableDefinition {
-        return {
-            headerTemplates: this.computeHeaderSpans(columnDirectives),
-            rowsTemplates: this.computeRowSpans(columnDirectives),
-            footerTemplates: this.computeFooterSpans(columnDirectives),
-        }
-    }
-
-    private computeHeaderSpans(columnDirectives: DataTableColumnDirective[]): DataTableColumn[][] {
-        let templates: DataTableColumn[][] = this.computeTemplate(columnDirectives,
-            (columnDirective: DataTableColumnDirective) => {
-                return !!columnDirective.title || !!columnDirective.sortingKey || !!columnDirective.tooltip;
-            },
-            (columnDirective: DataTableColumnDirective, index: number) => {
-                return {
-                    title: columnDirective.title,
-                    sortingKey: columnDirective.sortingKey,
-                    tooltip: columnDirective.tooltip
-                }
-            });
-
-        if (templates) {
-            let isAvailable = templates.some((template: DataTableColumn[]) => {
-                return template.some((cell: DataTableColumn) => {
-                    return !!cell.title || !!cell.sortingKey || !!cell.tooltip;
-                });
-            });
-            if (isAvailable) {
-                return templates;
-            }
-        }
-    }
-
-    private computeRowSpans(columnDirectives: DataTableColumnDirective[]): DataTableColumn[][] {
-        let templates: DataTableColumn[][] = this.computeTemplate(columnDirectives,
-            (columnDirective: DataTableColumnDirective) => {
-                let cellTemplate: TemplateRef<any>;
-                if (columnDirective.cellTemplate && columnDirective.cellTemplate.length) {
-                    cellTemplate = columnDirective.cellTemplate.first;
-                }
-                return !!cellTemplate;
-            },
-            (columnDirective: DataTableColumnDirective, index: number) => {
-                let cellTemplate: TemplateRef<any>;
-                if (columnDirective.cellTemplate && columnDirective.cellTemplate.length) {
-                    cellTemplate = columnDirective.cellTemplate.first;
-                }
-                return {
-                    cellTemplate: cellTemplate
-                }
-            });
-
-        if (templates) {
-            let isAvailable = templates.some((template: DataTableColumn[]) => {
-                return template.some((cell: DataTableColumn) => {
-                    return !!cell.cellTemplate;
-                });
-            });
-            if (isAvailable) {
-                return templates;
-            }
-        }
-    }
-
-    private computeFooterSpans(columnDirectives: DataTableColumnDirective[]): DataTableColumn[][] {
-        let templates: DataTableColumn[][] = this.computeTemplate(columnDirectives,
-            (columnDirective: DataTableColumnDirective) => {
-                let footerTemplate: TemplateRef<any>;
-                if (columnDirective.footerTemplate && columnDirective.footerTemplate.length) {
-                    footerTemplate = columnDirective.footerTemplate.first;
-                }
-                return !!footerTemplate;
-            },
-            (columnDirective: DataTableColumnDirective, index: number) => {
-                let footerTemplate: TemplateRef<any>;
-                if (columnDirective.footerTemplate && columnDirective.footerTemplate.length) {
-                    footerTemplate = columnDirective.footerTemplate.first;
-                }
-                return {
-                    footerTemplate: footerTemplate
-                }
-            });
-
-        if (templates) {
-            let isAvailable = templates.some((template: DataTableColumn[]) => {
-                return template.some((cell: DataTableColumn) => {
-                    return !!cell.footerTemplate;
-                });
-            });
-            if (isAvailable) {
-                return templates;
-            }
-        }
-    }
-
-    private computeTemplate(columnDirectives: DataTableColumnDirective[],
-                            hasCell: (columnDirective: DataTableColumnDirective, index: number)
-                                => boolean,
-                            getCell: (columnDirective: DataTableColumnDirective, index: number)
-                                => DataTableColumn): DataTableColumn[][] {
-        let flatSubColumnsDataTableColumn: DataTableColumn[][][] = [];
-        let maxDepth = 0;
-        columnDirectives.forEach((columnDirective: DataTableColumnDirective) => {
-            let subColumns: DataTableColumn[][] = this.computeTemplate(columnDirective.subColumns.toArray().slice(1),
-                hasCell, getCell);
-            if (subColumns && subColumns.length) {
-                flatSubColumnsDataTableColumn.push(subColumns);
-                maxDepth = Math.max(maxDepth, subColumns.length);
-            } else {
-                flatSubColumnsDataTableColumn.push(null);
-            }
-        });
-
-        if (!columnDirectives || !columnDirectives.length) {
-            return;
-        }
-
-        let result: DataTableColumn[][] = [[]];
-        columnDirectives.forEach((columnDirective: DataTableColumnDirective, index: number) => {
-            if (hasCell(columnDirective, index)) {
-                let cell: DataTableColumn = getCell(columnDirective, index);
-                cell.rowspan = this.computeRowspan(maxDepth + 1, flatSubColumnsDataTableColumn[index]);
-                cell.colspan = this.computeColspan(columnDirective);
-                result[0].push(cell);
-            } else if (flatSubColumnsDataTableColumn[index]) {
-                // Merge cells from next row if needed
-                result[0] = result[0].concat(flatSubColumnsDataTableColumn[index][0]);
-                if (flatSubColumnsDataTableColumn[index].length > 1) {
-                    flatSubColumnsDataTableColumn[index] = flatSubColumnsDataTableColumn[index].slice(1);
-                } else {
-                    flatSubColumnsDataTableColumn[index] = null;
-                }
-            } else {
-                result[0].push({
-                    rowspan: this.computeRowspan(maxDepth + 1, flatSubColumnsDataTableColumn[index]),
-                    colspan: this.computeColspan(columnDirective)
-                });
-            }
-        });
-
-        // Compute new depth after merging cells
-        let newDepth = 0;
-        flatSubColumnsDataTableColumn.forEach((flatSubColumnsDataTable: DataTableColumn[][]) => {
-            if (flatSubColumnsDataTable) {
-                newDepth = Math.max(newDepth, flatSubColumnsDataTable.length);
-            }
-        });
-
-        // Fix rowspans after merging cells
-        result[0].forEach((cell: DataTableColumn) => {
-            if (cell.rowspan) {
-                cell.rowspan = cell.rowspan - (maxDepth - newDepth);
-                if (cell.rowspan < 1) {
-                    cell.rowspan = null;
-                }
-            }
-        });
-
-        // remove if empty
-        if (!result[0].length) {
-            result = [];
-        }
-
-        // Process sub tables
-        for (let i = 0; i < maxDepth; i++) {
-            result.push([]);
-            flatSubColumnsDataTableColumn.forEach((flatSubColumnsDataTable: DataTableColumn[][]) => {
-                if (flatSubColumnsDataTable && flatSubColumnsDataTable[i]) {
-                    result.push(result.pop().concat(flatSubColumnsDataTable[i]));
-                }
-            });
-
-            // remove if empty
-            if (!result[result.length - 1].length) {
-                result.pop();
-            }
-        }
-
-        if (result.length) {
-            return result;
-        }
-    }
-
-    private computeColspan(columnDirective: DataTableColumnDirective): number {
-        let subColumns: DataTableColumnDirective[] = columnDirective.subColumns.toArray().slice(1);
-        if (subColumns.length > 1) {
-            return subColumns.length;
-        }
-    }
-
-    private computeRowspan(rowspan: number, flatSubColumns: DataTableColumn[][]): number {
-        if (flatSubColumns) {
-            rowspan = rowspan - flatSubColumns.length;
-        }
-        if (rowspan > 1) {
-            return rowspan;
-        }
-    }
+    //</editor-fold>
 }
 
