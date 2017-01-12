d1f9019881607d663571480feb5832f2f3bec84c
Adam Netočný <adam@netocny.sk>
2017-01-13 00:19:56 +0100

    Fix missing generics when providing generic service and compiling with NGC

--- src/main/resources/webroot/app/app.module.ts
+++ src/main/resources/webroot/app/app.module.ts
@@ -1,17 +1,22 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-import {HttpModule, JsonpModule} from '@angular/http';
+import {HttpModule, JsonpModule, Http} from '@angular/http';
 
 import {HttpService} from './http.service';
 
 import {AppComponent} from './app.component';
 
 import {AuthModule} from './auth/auth.module';
+import {AuthHttp} from "angular2-jwt";
 
 import {MenuModule} from './menu/menu.module';
 import {RoutingModule} from './routes/routing.module';
 
 import {TradingModule} from './trading/trading.module';
+
+export function HttpServiceFactory(http: Http, authHttp: AuthHttp): HttpService<any> {
+    return new HttpService(http, authHttp);
+}
 
 @NgModule({
     imports: [
@@ -27,7 +32,12 @@ import {TradingModule} from './trading/trading.module';
         AppComponent
     ],
     providers: [
-        HttpService
+        {
+            provide: HttpService,
+            deps: [Http, AuthHttp],
+            useFactory: HttpServiceFactory
+        }
+
     ],
     bootstrap: [
         AppComponent
