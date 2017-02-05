7d36878cad0e45fb4e1f612792598f4867911b8c
Jakub Scholz <www@scholzj.com>
2017-02-05 16:34:49 +0100

    Big code cleanup #54 #11

--- src/main/java/com/deutscheboerse/risk/dave/MainVerticle.java
+++ src/main/java/com/deutscheboerse/risk/dave/MainVerticle.java
@@ -1,12 +1,17 @@
 package com.deutscheboerse.risk.dave;
 
-import io.vertx.core.*;
-import io.vertx.core.json.JsonArray;
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.CompositeFuture;
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Future;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by schojak on 19.8.16.
@@ -15,20 +20,14 @@ public class MainVerticle extends AbstractVerticle {
     private static final Logger LOG = LoggerFactory.getLogger(MainVerticle.class);
 
     private String mongoDbPersistenceDeployment;
-    private String ersDebbugerDeployment;
-    private List<String> ersConnectorDeployment = new ArrayList<>();
     private String httpDeployment;
-    private String masterdataDeployment;
 
     @Override
     public void start(Future<Void> startFuture) {
         Future<Void> chainFuture = Future.future();
         deployMongoDBVerticle()
-                .compose(this::deployMasterData)
-                .compose(this::deployERSDebuggerVerticle)
                 .compose(this::deployHttpsVerticle)
                 .compose(this::deployHttpVerticle)
-                .compose(this::deployErsVerticles)
                 .compose(chainFuture::complete, chainFuture);
 
         chainFuture.setHandler(ar -> {
@@ -56,25 +55,6 @@ public class MainVerticle extends AbstractVerticle {
             }
         });
         return mongoDbVerticleFuture;
-    }
-
-    private Future<Void> deployERSDebuggerVerticle(Void unused) {
-        Future<Void> ersDebuggerVerticleFuture = Future.future();
-        if (config().getJsonObject("ersDebugger", new JsonObject()).getBoolean("enable", false)) {
-            DeploymentOptions ersDebuggerOptions = new DeploymentOptions().setConfig(config().getJsonObject("ersDebugger"));
-            vertx.deployVerticle(ERSDebuggerVerticle.class.getName(), ersDebuggerOptions, ar -> {
-                if (ar.succeeded()) {
-                    LOG.info("Deployed ERSDebuggerVerticle with ID {}", ar.result());
-                    ersDebbugerDeployment = ar.result();
-                    ersDebuggerVerticleFuture.complete();
-                } else {
-                    ersDebuggerVerticleFuture.fail(ar.cause());
-                }
-            });
-        } else {
-            ersDebuggerVerticleFuture.complete();
-        }
-        return ersDebuggerVerticleFuture;
     }
 
     private Future<Void> deployHttpsVerticle(Void unused) {
@@ -116,74 +96,10 @@ public class MainVerticle extends AbstractVerticle {
         return httpVerticleFuture;
     }
 
-    private Future<Void> deployErsVerticles(Void unused)
-    {
-        Future<Void> ersConnectorDeploymentFuture = Future.future();
-        List<Future> tasks = new ArrayList<>();
-
-        JsonArray ersConnectorOptions = config().getJsonArray("ers");
-        ersConnectorOptions.forEach( connectorOptions -> {
-            LOG.info("Deploying ERS connector verticle {}", (JsonObject)connectorOptions);
-
-            DeploymentOptions ersDeploymentOptions = new DeploymentOptions().setConfig((JsonObject)connectorOptions);
-            Future<String> fut = Future.future();
-
-            vertx.deployVerticle(ERSConnectorVerticle.class.getName(), ersDeploymentOptions, res -> {
-                if (res.succeeded()) {
-                    LOG.info("Deployed ERSConnectorVerticle with ID {}", res.result());
-                    ersConnectorDeployment.add(res.result());
-                    fut.complete();
-                } else {
-                    LOG.error("Failed to deployed ERSConnectorVerticle", res.cause());
-                    fut.fail(res.cause());
-                }
-            });
-            tasks.add(fut);
-        });
-
-        CompositeFuture.all(tasks).setHandler(ar -> {
-            if (ar.succeeded()) {
-                LOG.info("Deployed all ERS verticles");
-                ersConnectorDeploymentFuture.complete();
-            } else {
-                LOG.error("Failed to deploy some or all ERS verticles");
-                ersConnectorDeploymentFuture.fail(ar.cause());
-            }
-        });
-        return ersConnectorDeploymentFuture;
-    }
-
-    private Future<Void> deployMasterData(Void unused) {
-        Future<Void> masterdataVerticleFuture = Future.future();
-        JsonObject config = config().getJsonObject("masterdata", new JsonObject());
-
-        if (config().getJsonObject("httpProxy") != null)
-        {
-            config.put("httpProxy", config().getJsonObject("httpProxy"));
-        }
-
-        DeploymentOptions masterdataOptions = new DeploymentOptions().setConfig(config);
-        vertx.deployVerticle(MasterdataVerticle.class.getName(), masterdataOptions, ar -> {
-            if (ar.succeeded()) {
-                LOG.info("Deployed MasterdataVerticle with ID {}", ar.result());
-                masterdataDeployment = ar.result();
-                masterdataVerticleFuture.complete();
-            } else {
-                masterdataVerticleFuture.fail(ar.cause());
-            }
-        });
-        return masterdataVerticleFuture;
-    }
-
     private void closeAllDeployments() {
         LOG.info("Undeploying verticles");
         Map<String, String> deployments = new HashMap<>();
-        ersConnectorDeployment.forEach(id -> {
-            deployments.put("ERSConnector", id);
-        });
-        deployments.put("ERSDebugger", ersDebbugerDeployment);
         deployments.put("HttpInterface", httpDeployment);
-        deployments.put("Masterdata", masterdataDeployment);
 
         List<Future> futures = new LinkedList<>();
         deployments.forEach((verticleName, deploymentID) -> {
--- src/test/java/com/deutscheboerse/risk/dave/MainVerticleIT.java
+++ src/test/java/com/deutscheboerse/risk/dave/MainVerticleIT.java
@@ -1,12 +1,11 @@
 package com.deutscheboerse.risk.dave;
 
-import com.deutscheboerse.risk.dave.utils.AutoCloseableConnection;
 import com.deutscheboerse.risk.dave.utils.DummyData;
-import com.deutscheboerse.risk.dave.utils.Utils;
 import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
+import io.vertx.ext.mongo.MongoClient;
 import io.vertx.ext.unit.Async;
 import io.vertx.ext.unit.TestContext;
 import io.vertx.ext.unit.junit.VertxUnitRunner;
@@ -15,81 +14,53 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import javax.jms.JMSException;
-import javax.jms.MessageProducer;
-import javax.jms.Session;
-import javax.naming.NamingException;
+import java.util.UUID;
 
 @RunWith(VertxUnitRunner.class)
 public class MainVerticleIT {
     private static Vertx vertx;
-    private static int tcpPort;
-    private static int sslPort;
     private static int httpPort;
     private static int mongoPort;
+    private static MongoClient mongoClient;
 
     @BeforeClass
     public static void setUp(TestContext context) {
         vertx = Vertx.vertx();
 
         httpPort = Integer.getInteger("http.port", 8080);
-        tcpPort = Integer.getInteger("ers.tcpport", 5672);
-        sslPort = Integer.getInteger("ers.sslport", 5671);
         mongoPort = Integer.getInteger("mongodb.port", 27017);
 
         JsonObject config = new JsonObject();
-        config.put("ers", new JsonArray().add(new JsonObject().put("brokerHost", "localhost").put("brokerPort", sslPort).put("member", "ABCFR").put("sslCertAlias", "abcfr").put("truststore", MainVerticleIT.class.getResource("ers.truststore").getPath()).put("truststorePassword", "123456").put("keystore", MainVerticleIT.class.getResource("ers.keystore").getPath()).put("keystorePassword", "123456")));
         config.put("http", new JsonObject().put("httpPort", httpPort));
         config.put("mongodb", new JsonObject().put("dbName", "DAVe-MainVerticleTest").put("connectionUrl", "mongodb://localhost:" + mongoPort));
-        config.put("ersDebugger", new JsonObject().put("enable", true));
-
-        //config.put("masterdata", new JsonObject().put("clearers", new JsonArray()));
-        JsonObject clearerABCFR  =new JsonObject().put("clearer", "ABCFR").put("members", new JsonArray().add(new JsonObject().put("member", "ABCFR").put("accounts", new JsonArray().add("A1").add("A2").add("PP"))).add(new JsonObject().put("member", "GHIFR").put("accounts", new JsonArray().add("PP").add("MY"))));
-        JsonObject clearerDEFFR = new JsonObject().put("clearer", "DEFFR").put("members", new JsonArray().add(new JsonObject().put("member", "DEFFR").put("accounts", new JsonArray().add("A1").add("A2").add("PP"))));
-        config.put("masterdata", new JsonObject().put("clearers", new JsonArray().add(clearerABCFR).add(clearerDEFFR)).put("productList", new JsonArray().add("JUN3").add("1COF").add("1COV")));
-
         vertx.deployVerticle(MainVerticle.class.getName(), new DeploymentOptions().setConfig(config), context.asyncAssertSuccess());
+
+        JsonObject dbConfig = new JsonObject();
+        dbConfig.put("db_name", "DAVe-Test" + UUID.randomUUID().getLeastSignificantBits());
+        dbConfig.put("connection_string", "mongodb://localhost:" + System.getProperty("mongodb.port", "27017"));
+        MainVerticleIT.mongoClient = MongoClient.createShared(MainVerticleIT.vertx, dbConfig);
     }
 
-    private void sendErsBroadcast(TestContext context, String routingKey, String messageBody) {
-        final Async asyncBroadcast = context.async();
+    private void sendDummyData(TestContext context) {
+        DummyData.positionReportJson.forEach(pr -> {
+            final Async asyncSend = context.async();
+            vertx.eventBus().send("ers.PositionReport", pr, ar -> {
+                context.assertTrue(ar.succeeded());
+                asyncSend.complete();
+            });
 
-        vertx.executeBlocking(future -> {
-            try {
-                Utils utils = new Utils();
-                AutoCloseableConnection conn = utils.getAdminConnection("localhost", tcpPort);
-                Session ses = conn.createSession(false, Session.CLIENT_ACKNOWLEDGE);
-                MessageProducer prod = ses.createProducer(utils.getTopic("eurex.broadcast/" + routingKey));
-                prod.send(ses.createTextMessage(messageBody));
-
-                future.complete();
-            }
-            catch (NamingException | JMSException e)
-            {
-                future.fail(e);
-            }
-        }, res -> {
-            if (res.succeeded())
-            {
-                asyncBroadcast.complete();
-            }
-            else
-            {
-                context.fail("Failed to send broadcast message");
-            }
+            asyncSend.awaitSuccess();
         });
-
-        asyncBroadcast.awaitSuccess();
     }
 
     @Test
     public void testPositionReport(TestContext context) throws InterruptedException {
-        sendErsBroadcast(context, "ABCFR.MessageType.Position", DummyData.positionReportXML);
+        sendDummyData(context);
 
         Thread.sleep(1000);
 
         final Async asyncRest = context.async();
-        vertx.createHttpClient().getNow(httpPort, "localhost", "/api/v1.0/pr/latest/ABCFR/DEFFR/A1/*/BMW/C/3500/1/201001", res -> {
+        vertx.createHttpClient().getNow(httpPort, "localhost", "/api/v1.0/pr/latest/ABCFR/ABCFR/A1/*/OGBL/C/10800/0/201401", res -> {
             context.assertEquals(200, res.statusCode());
             res.bodyHandler(body -> {
                 try {
@@ -100,17 +71,17 @@ public class MainVerticleIT {
                     JsonObject pos = positions.getJsonObject(0);
 
                     context.assertEquals("ABCFR", pos.getString("clearer"));
-                    context.assertEquals("DEFFR", pos.getString("member"));
+                    context.assertEquals("ABCFR", pos.getString("member"));
                     context.assertNull(pos.getString("reqID"));
                     context.assertEquals("A1", pos.getString("account"));
-                    context.assertEquals("ITD", pos.getString("sesId"));
-                    context.assertEquals("13365938226608", pos.getString("rptId"));
+                    //context.assertEquals("ITD", pos.getString("sesId"));
+                    context.assertEquals("13198434645156", pos.getString("rptId"));
                     context.assertEquals("C", pos.getString("putCall"));
-                    context.assertEquals("201001", pos.getString("maturityMonthYear"));
-                    context.assertEquals("3500", pos.getString("strikePrice"));
-                    context.assertEquals("BMW", pos.getString("symbol"));
-                    context.assertEquals(0.0, pos.getDouble("crossMarginLongQty"));
-                    context.assertEquals(100.0, pos.getDouble("crossMarginShortQty"));
+                    context.assertEquals("201401", pos.getString("maturityMonthYear"));
+                    context.assertEquals("10800", pos.getString("strikePrice"));
+                    context.assertEquals("OGBL", pos.getString("symbol"));
+                    context.assertEquals(700.0, pos.getDouble("crossMarginLongQty"));
+                    context.assertEquals(800.0, pos.getDouble("crossMarginShortQty"));
                     context.assertEquals(0.0, pos.getDouble("optionExcerciseQty"));
                     context.assertEquals(0.0, pos.getDouble("optionAssignmentQty"));
                     context.assertNull(pos.getDouble("allocationTradeQty"));
