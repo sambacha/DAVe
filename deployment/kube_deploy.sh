2fecf9e75c8b43974f1eab8b1e3c544b7de574f8
Jakub Scholz <www@scholzj.com>
2017-04-04 22:22:15 +0200

    Use dbgdave/dave-api as the docker image

--- deployment/kube_deploy.sh
+++ deployment/kube_deploy.sh
@@ -8,4 +8,4 @@ chmod +x kubectl
 #./kubectl config set-credentials ${KUBE_USERNAME} --token ${KUBE_PASSWORD}
 ./kubectl config set-context default-context --cluster=dave --user=dave
 ./kubectl config use-context default-context
-./kubectl set image deployment/dave-api dave-api=scholzj/dave:${CIRCLE_SHA1} --namespace=${CIRCLE_BRANCH}
+./kubectl set image deployment/dave-api dave-api=dbgdave/dave-api:${CIRCLE_SHA1} --namespace=${CIRCLE_BRANCH}
