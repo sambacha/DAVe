be75b6ba0e9c84e7ab60a4ae6bfff74705913519
Josef Hala <josef.hala@deutsche-boerse.com>
2017-04-13 13:44:46 +0200

    Cover implicit type conversion by tests

--- src/test/java/com/deutscheboerse/risk/dave/MainVerticleTest.java
+++ src/test/java/com/deutscheboerse/risk/dave/MainVerticleTest.java
@@ -1,12 +1,15 @@
 package com.deutscheboerse.risk.dave;
 
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import com.deutscheboerse.risk.dave.log.TestAppender;
 import com.deutscheboerse.risk.dave.model.PositionReportModel;
 import com.deutscheboerse.risk.dave.persistence.EchoPersistenceService;
 import com.deutscheboerse.risk.dave.persistence.PersistenceService;
 import com.deutscheboerse.risk.dave.persistence.StoreManagerMock;
+import com.deutscheboerse.risk.dave.util.URIBuilder;
 import com.deutscheboerse.risk.dave.utils.DataHelper;
 import com.deutscheboerse.risk.dave.utils.TestConfig;
-import com.deutscheboerse.risk.dave.util.URIBuilder;
 import com.google.inject.AbstractModule;
 import com.google.inject.Singleton;
 import io.vertx.core.DeploymentOptions;
@@ -21,16 +24,21 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.LoggerFactory;
 
 import java.io.UnsupportedEncodingException;
 
 @RunWith(VertxUnitRunner.class)
 public class MainVerticleTest {
+    private static final TestAppender testAppender = TestAppender.getAppender(MainVerticle.class);
+    private static final Logger rootLogger = (Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
+
     private Vertx vertx;
 
     @Before
     public void setUp() {
         this.vertx = Vertx.vertx();
+        rootLogger.addAppender(testAppender);
     }
 
     @Test
@@ -83,6 +91,25 @@ public class MainVerticleTest {
     }
 
     @Test
+    public void testImplicitTypeConversion(TestContext context) throws InterruptedException {
+        DeploymentOptions options = getDeploymentOptions();
+
+        options.getConfig()
+                .getJsonObject("storeManager", new JsonObject())
+                    .put("port", String.valueOf(TestConfig.STORE_MANAGER_PORT))
+                    .put("verifyHost", "false");
+
+        Level rootLevel = rootLogger.getLevel();
+        rootLogger.setLevel(Level.DEBUG);
+        testAppender.start();
+        vertx.deployVerticle(MainVerticle.class.getName(), options, context.asyncAssertSuccess());
+        testAppender.waitForMessageContains(Level.DEBUG, "\"port\" : " + String.valueOf(TestConfig.STORE_MANAGER_PORT));
+        testAppender.waitForMessageContains(Level.DEBUG, "\"verifyHost\" : false");
+        testAppender.stop();
+        rootLogger.setLevel(rootLevel);
+    }
+
+    @Test
     public void testFailedDeployment(TestContext context) {
         DeploymentOptions options = getDeploymentOptions();
         options.getConfig().getJsonObject("api", new JsonObject()).put("port", -1);
@@ -112,6 +139,8 @@ public class MainVerticleTest {
 
     @After
     public void cleanup(TestContext context) {
+        rootLogger.detachAppender(testAppender);
+        testAppender.clear();
         this.vertx.close(context.asyncAssertSuccess());
     }
 
