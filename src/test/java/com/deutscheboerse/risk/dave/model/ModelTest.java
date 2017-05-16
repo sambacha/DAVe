9a8792d18c69d0bfd3311f54e49acff66d8cd7c0
Josef Hala <josef.hala@deutsche-boerse.com>
2017-05-16 12:54:04 +0200

    Test corrupted models #91

--- src/test/java/com/deutscheboerse/risk/dave/model/ModelTest.java
+++ src/test/java/com/deutscheboerse/risk/dave/model/ModelTest.java
@@ -13,4 +13,34 @@ public class ModelTest {
     public void testCorruptedJson(TestContext context) {
         new AccountMarginModel(new JsonObject());
     }
+
+    @Test(expected = RuntimeException.class)
+    public void testCorruptedAccountMarginModel(TestContext context) {
+        new AccountMarginModel(new JsonObject().put("grpc", new byte[]{0}));
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testCorruptedLiquiGroupMarginModel(TestContext context) {
+        new LiquiGroupMarginModel(new JsonObject().put("grpc", new byte[]{0}));
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testCorruptedLiquiGroupSplitMarginModel(TestContext context) {
+        new LiquiGroupSplitMarginModel(new JsonObject().put("grpc", new byte[]{0}));
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testCorruptedPoolMarginModel(TestContext context) {
+        new PoolMarginModel(new JsonObject().put("grpc", new byte[]{0}));
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testCorruptedPositionReportModel(TestContext context) {
+        new PositionReportModel(new JsonObject().put("grpc", new byte[]{0}));
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testRiskLimitUtilizationModel(TestContext context) {
+        new RiskLimitUtilizationModel(new JsonObject().put("grpc", new byte[]{0}));
+    }
 }
