723e242393e734504a8d378144843bf8df5790aa
Michal Zerola <michal.zerola@gmail.com>
2017-08-15 16:48:55 +0200

    Removing JWT configuration from output when starting a verticle. #109

--- docker/create_docker.sh
+++ docker/create_docker.sh
@@ -7,10 +7,11 @@ DAVE_CONFIG_FILE="${DAVE_APPLICATION_NAME}-${DAVE_VERSION}/etc/dave.conf"
 # Copy the DAVe binaries
 cp -r -v ./target/"${DAVE_APPLICATION_NAME}-${DAVE_VERSION}"/"${DAVE_APPLICATION_NAME}-${DAVE_VERSION}" ./docker/"${DAVE_APPLICATION_NAME}-${DAVE_VERSION}"
 
+sed -i 's/\(\s\+wellKnownUrl\s\+=\).*/\1\ \"\"/' ./docker/"${DAVE_CONFIG_FILE}"
+sed -i 's/\(\s\+clientId\s\+=\).*/\1\ \"\"/' ./docker/"${DAVE_CONFIG_FILE}"
 sed -i 's/\(\s\+sslKey\s\+=\).*/\1\ \"\"/' ./docker/"${DAVE_CONFIG_FILE}"
 sed -i 's/\(\s\+sslCert\s\+=\).*/\1\ \"\"/' ./docker/"${DAVE_CONFIG_FILE}"
 sed -i 's/\(\s\+sslTrustCerts\s\+=\).*/\1\ \[\]/' ./docker/"${DAVE_CONFIG_FILE}"
-sed -i 's/\(\s\+jwtPublicKey\s\+=\).*/\1\ \[\]/' ./docker/"${DAVE_CONFIG_FILE}"
 
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 docker build -t dbgdave/dave-api:${CIRCLE_SHA1} ./docker/
